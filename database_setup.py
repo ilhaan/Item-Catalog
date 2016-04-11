89bd883f5f1366c65202fcafcdadee43f14173b3
Ilhaan Rasheed <ilhaan@yahoo.com>
2016-04-11 00:28:21 -0400

    Add database setup script

---
+++ database_setup.py
@@ -0,0 +1,61 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+    name = Column(String(80), primary_key = True)
+    email = Column(String(80), nullable = False)
+    picture = Column(String(250), nullable = False)
+    id = Column(Integer, primary_key = True)
+
+    @property
+    def serialize(self):
+        return {
+            'name' : self.name,
+            'email' : self.email,
+            'picture' : self.picture,
+            'id' : self.id,
+        }
+
+class Category(Base):
+    __tablename__ = 'category'
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'name' : self.name,
+            'id' : self.id,
+            'user_id' : self.user_id,
+        }
+
+class Item(Base):
+    __tablename__ = 'item'
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'name' : self.name,
+            'description' : self.description,
+            'id' : self.id,
+            'user_id' : self.user_id,
+        }
+
+engine = create_engine('sqlite:///itemcatalog.db')
+
+Base.metadata.create_all(engine)
