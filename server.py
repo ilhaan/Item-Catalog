6810400e62a7aa88df4f463029678f1ba7c9bcb8
Ilhaan Rasheed <ilhaan@yahoo.com>
2016-06-27 23:39:31 -0400

    Add capability to create new categories and display them

--- server.py
+++ server.py
@@ -11,9 +11,18 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+@app.route('/login/')
+def showLogin():
+    # state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    # login_session['state'] = state
+    # return render_template('login.html', STATE=state)
+    return "This is the login page"
+
 @app.route('/')
 def showCategories():
-    return "This is the main page. All categories will be listed here."
+    categories = session.query(Category).all();
+    return render_template('categories.html', categories = categories)
+    # return "This is the main page. All categories will be listed here."
 
 @app.route('/category/<int:category_id>')
 def showItemList(category_id):
@@ -23,6 +32,22 @@ def showItemList(category_id):
 def showItem(category_id, item_id):
     return "This page will have a description of the %s item under the %s \
             category." % (category_id, item_id)
+
+@app.route('/category/new/', methods = ['GET', 'POST'])
+def newCategory():
+    # if 'username' not in login_session:
+    #     return redirect('/login/')
+    if request.method == 'POST':
+        # newCategory = Category(name = request.form['name'], user_id = login_session['user_id'])
+        newCategory = Category(name = request.form['name'])
+        session.add(newCategory)
+        session.commit()
+        flash("New category created!")
+        return redirect(url_for('showCategories'))
+    else:
+        return render_template('newcategory.html')
+        # return render_template('newcategory.html', login_session = login_session)
+        # return "This is the add new item page"
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
