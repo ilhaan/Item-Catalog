971c5e85e5b816305efa7c31e2eeeba81f5c303e
Ilhaan Rasheed <ilhaan@yahoo.com>
2016-07-09 15:07:13 -0400

    Disable debug mode

--- server.py
+++ server.py
@@ -382,5 +382,5 @@ def getUserInfo(user_id):
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
-    app.debug = True
+    # app.debug = True
     app.run(host='0.0.0.0', port=5000)
